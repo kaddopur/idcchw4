68641b7aaecaaff08f56fa88fa004c1140e2a6be
Jason C. Huang <chaoju.huang@gmail.com>
2012-05-11 03:44:35 +0800

    facebook login & logout

--- js/index.js
+++ js/index.js
@@ -20,12 +20,13 @@ display_all = function(msg) {
   for (_i = 0, _len = msg_json.length; _i < _len; _i++) {
     g = msg_json[_i];
     author_to_display = g.author ? g.author : 'An anonymous person';
-    _results.push($('#display_area').append("<div class='post'><button class='close' onclick='delete_post(\"" + g.key + "\")'>&times;</button><b>" + author_to_display + "</b> wrote at " + g.date + ":<blockquote>" + g.content + "</blockquote></div>"));
+    _results.push($('#display_area').append("<div class='post'><button class='close' onclick='delete_post(\"" + g.key + "\")'>&times;</button><b>" + author_to_display + "</b> wrote at <span class='label label-info'>" + g.date + "</span><blockquote>" + g.content + "</blockquote></div>"));
   }
   return _results;
 };
 
 $('#send').click(function() {
+  if (!$('textarea').val()) return;
   return $.ajax({
     url: '/sign',
     type: 'POST',
@@ -37,8 +38,34 @@ $('#send').click(function() {
   });
 });
 
+$('#login').click(function() {
+  return FB.login(function(response) {
+    if (response.authResponse) {
+      console.log('Welcome!  Fetching your information.... ');
+      return FB.api('/me', function(response) {
+        console.log('Good to see you, ' + response.name + '.');
+        $('#profile').attr('src', "https://graph.facebook.com/" + response.id + "/picture");
+        $('#login').hide();
+        return $('#logout').show();
+      });
+    } else {
+      return console.log('User cancelled login or did not fully authorize.');
+    }
+  });
+});
+
+$('#logout').click(function() {
+  return FB.logout(function(response) {
+    $('#profile').attr('src', "img/yetlogin.png");
+    $('#login').show();
+    return $('#logout').hide();
+  });
+});
+
 $.ajax({
   url: '/posts'
 }).done(function(msg) {
   return display_all(msg);
 });
+
+$('#logout').hide();
--- src/index.coffee
+++ src/index.coffee
@@ -13,10 +13,12 @@ display_all = (msg)->
   $('#display_area').html('')
   for g in msg_json
     author_to_display = if g.author then g.author else 'An anonymous person'
-    $('#display_area').append("<div class='post'><button class='close' onclick='delete_post(\"#{g.key}\")'>&times;</button><b>#{author_to_display}</b> wrote at #{g.date}:<blockquote>#{g.content}</blockquote></div>")
+    $('#display_area').append("<div class='post'><button class='close' onclick='delete_post(\"#{g.key}\")'>&times;</button><b>#{author_to_display}</b> wrote at <span class='label label-info'>#{g.date}</span><blockquote>#{g.content}</blockquote></div>")
+
 
 # upload post & refresh
 $('#send').click ->
+  return unless $('textarea').val()
   $.ajax({
     url: '/sign',
     type: 'POST',
@@ -26,11 +28,36 @@ $('#send').click ->
   }).done (msg) ->
     display_all(msg)
 
+
+# fb login
+$('#login').click ->
+  FB.login (response) ->
+    if response.authResponse
+      console.log('Welcome!  Fetching your information.... ')
+      FB.api('/me', (response) ->
+        console.log('Good to see you, ' + response.name + '.')
+        $('#profile').attr('src', "https://graph.facebook.com/#{response.id}/picture")
+        $('#login').hide()
+        $('#logout').show()
+      )
+    else
+      console.log('User cancelled login or did not fully authorize.')
+  
+# fb logout
+$('#logout').click ->
+  FB.logout (response) ->
+    $('#profile').attr('src', "img/yetlogin.png")
+    $('#login').show()
+    $('#logout').hide()
+  
+
+
+
+
 # load posts
 $.ajax({
   url: '/posts'
 }).done (msg) ->
   display_all(msg)
-
-
+$('#logout').hide()
 
